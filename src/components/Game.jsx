05a59e0709dca6b003bf6cb6f179e3535bcdd34d
Eder Henrique <ederhscc@gmail.com>
2024-07-23 18:30:43 -0300

    generating card and shuffle them

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,6 +1,42 @@
+import { useState } from "react";
 import Board from "./Board";
 
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    
+    const j = Math.floor(Math.random() * (i + 1));
+    
+    [array[i], array[j]] = [array[j], array[i]]
+  }
+
+  return array;
+};
+
+const generateCards = () => {
+  const values = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+  const cards = values.map((value) => ({
+    value,
+    isFlipped: false,
+  }));
+
+  const duplicatedCards = cards
+    .concat([...cards])
+    .map((card, index) => ({ ...card, index }));
+
+  console.log(shuffleArray(duplicatedCards));
+};
+
+generateCards();
+
 const Game = () => {
+
+    const [cards, setCards] = useState(generateCards)
+
+    const [flippedCards, setFlippedCards] = useState([])
+
+    const [chances, setChances] = useState(6)
+
   return (
     <div className="game">
       <Board />
