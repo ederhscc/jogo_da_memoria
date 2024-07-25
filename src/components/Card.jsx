cee8a1e804e10bfdb3c53d3eb32d90b3a39a4b00
Eder Henrique <ederhscc@gmail.com>
2024-07-25 13:15:49 -0300

    finalizing project

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
-const Card = ({card}) => {
+const Card = ({card, onClick}) => {
   return (
-    <div className={`card ${card.isFlepped ? "flipped" : ""}`}>
-        {card.isFlepped ? card.value : "?"}
+    <div className={`card ${card.isFlipped ? "flipped" : ""}`} onClick={() => onClick(card)}>
+        {card.isFlipped ? card.value : "?"}
     </div>
   )
 }
