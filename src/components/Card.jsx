2b18235518891a4dfafb56bf60480371daaf1117
Eder Henrique <ederhscc@gmail.com>
2024-07-24 22:39:42 -0300

    added project's styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = ({card}) => {
   return (
     <div className={`card ${card.isFlepped ? "flipped" : ""}`}>
-        {card.value}
+        {card.isFlepped ? card.value : "?"}
     </div>
   )
 }
